dcff5050c8e0521add0552715f44f86b6b546920
Nepsaco <tobie.tsuzuki@gmail.com>
2020-01-28 13:01:03 -0700

    playing with digits completed

--- playingWithDigits.js
+++ playingWithDigits.js
@@ -3,7 +3,8 @@
 // 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
 // 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 // Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
-// we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive 
+// we want to find a positive integer k, if it exists, such 
+// as the sum of the digits of n taken to the successive 
 // powers of p is equal to k * n.
 // In other words:
 // Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
@@ -12,25 +13,26 @@
 
 function digitPower( number, power ) {
     const numberArray = number.toString().split('')
-        console.log(numberArray)
     let powerNumber = 0
     for(i=0; i < numberArray.length; i++){
-        console.log(numberArray[i])
-        powerNumber = parseInt(numberArray[i])  ** power
+        powerNumber += Math.pow(parseInt(numberArray[i]), power)
         power++
-        return powerNumber
     }
-    console.log(powerNumber)
+    if(powerNumber % number === 0){
+        return 1
+    } else {
+        return -1
+    }
 }
 
-digitPower(89, 1) 
+// console.log(digitPower(89, 1) )
 // should return 1 since 8¹ + 9² = 89 = 89 * 1
 
-// digitPower(92, 1) 
+// console.log(digitPower(92, 1) )
 // should return -1 since there is no k such as 9¹ + 2² equals 92 * k
 
-// digitPower(695, 2) 
+// console.log(digitPower(695, 2) )
 // should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 
-// digitPower(46288, 3) 
+// console.log(digitPower(46288, 3) )
 // should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
