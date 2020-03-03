d3c977d8d269c34246e5de548b64c8a8279e93cf
Nepsaco <tobie.tsuzuki@gmail.com>
2020-03-03 13:32:23 -0700

    added are they the same

---
+++ areTheyTheSame.js
@@ -0,0 +1,12 @@
+// Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) 
+// that checks whether the two arrays have the "same" elements, 
+// with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, 
+// regardless of the order.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+function comp(array, arraySquared){
+    return true 
+}
+
+console.log(comp(a,b))
