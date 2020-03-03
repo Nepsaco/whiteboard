57e564de1aa2e8d46028209a82e5df44fb33eb20
Nepsaco <tobie.tsuzuki@gmail.com>
2020-03-03 14:42:37 -0700

    changed title

--- areTheyTheSame.js
+++ areTheyTheSame.js
@@ -5,7 +5,7 @@
 
 a = [121, 144, 19, 161, 19, 144, 19, 11]
 b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
-function comp(array, arraySquared){
+function comp(array, arrayMultiplyed){
     return true 
 }
 
