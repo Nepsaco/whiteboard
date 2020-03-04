098a77b6a8614d6f4dca87306c5288c8a159bcff
Nepsaco <tobie.tsuzuki@gmail.com>
2020-03-03 17:39:19 -0700

    solved are they the same

--- areTheyTheSame.js
+++ areTheyTheSame.js
@@ -3,10 +3,23 @@
 // with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, 
 // regardless of the order.
 
-a = [121, 144, 19, 161, 19, 144, 19, 11]
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
 b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
-function comp(array, arrayMultiplyed){
-    return true 
+function comp(array, arrayMultiplied){
+    if(array == null || arrayMultiplied == null){
+        return false
+    } 
+    array.sort((a, b) => a-b)
+    arrayMultiplied.sort((a, b) => a-b)
+    let i = 0
+    return array.reduce((sameArray, number) => {
+        if((number * number) === arrayMultiplied[i]){
+            i++
+            return true
+        } else {
+            return false
+        }
+    }, false)
 }
 
 console.log(comp(a,b))
