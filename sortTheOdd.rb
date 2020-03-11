1d769b5db8e9ea19b606a5fbb542a57bb4c80846
Nepsaco <tobie.tsuzuki@gmail.com>
2020-03-11 17:29:48 -0600

    solved sort odds

--- sortTheOdd.rb
+++ sortTheOdd.rb
@@ -4,6 +4,10 @@
 # Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
 
 
-def sort_array(sorce_array)
+def sort_array(source_array)
+  odd_array = source_array.select{|number| number.odd?}.sort  
+  source_array.map{ |number| number.even? ? number : odd_array.shift }
 
 end
+
+puts sort_array([5, 3, 2, 8, 1, 4, 11])
