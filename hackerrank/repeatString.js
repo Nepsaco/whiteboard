d39afa95b6519b13d611ab8573d3aca626100917
Nepsaco <tobie.tsuzuki@gmail.com>
2020-05-05 14:05:43 -0600

    solved repeatString

--- hackerrank/repeatString.js
+++ hackerrank/repeatString.js
@@ -1,10 +1,29 @@
-function repeatedString(s, n, newString='') {
-    let index = 0
+// function repeatedString(s, n) {
+//     let string = ''
 
-    console.log(newString)
-    return newString.length < n
-        ? repeatedString(s, n, newString + s)
-        : newString.split('').filter(letter => letter === 'a').length
+     
+//     while (string.length < n){
+//         string = string + s
+//     }
+
+//     return string
+//         .split('')
+//         .slice(0, n)
+//         .filter(letter => letter === 'a')
+//         .length
+// }
+function repeatedString(s, n) {
+    const sRepeated = Math.floor(n / s.length)
+    let count = (s.split('a').length - 1) * sRepeated
+    let remainder = n % s.length
+
+    for(let i = 0; i < remainder; i++){
+        if(s.charAt(i) === 'a'){
+            count++
+        }
+    }
+
+    return count
 }
 
-console.log(repeatedString('a', 100000000000))
+console.log(repeatedString('aba', 10000000))
